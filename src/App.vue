a7c0354f96066fa8c922e781180e16b63c116dc2
gitInterface <97436826+gitInterface@users.noreply.github.com>
2025-07-22 03:16:09 +0800

    remove video outer element

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,23 @@
     <!-- 視訊畫面 -->
     <div class="flex flex-col md:flex-row justify-center items-stretch gap-0 w-full px-0">
       <!-- 本地視訊 -->
-      <div class="w-full flex justify-center">
-        <div class="relative w-full md:w-[48%] max-w-[500px] aspect-video min-w-0">
-          <video ref="localVideo" class="w-full h-full object-cover bg-black rounded-xl shadow overflow-hidden" autoplay
-            playsinline muted></video>
-          <!-- <button v-if="localStream" @click="enterFullscreen(localVideo.value)"
+      <div class="relative w-full md:w-[48%] max-w-[500px] aspect-video min-w-0">
+        <video ref="localVideo" class="w-full h-full object-cover bg-black rounded-xl shadow overflow-hidden" autoplay
+          playsinline muted></video>
+        <!-- <button v-if="localStream" @click="enterFullscreen(localVideo.value)"
             class="absolute bottom-2 right-2 bg-white bg-opacity-70 text-black text-base px-4 py-2 rounded-xl hover:bg-opacity-90 shadow transition">
             ⛶ 全螢幕
           </button> -->
-        </div>
       </div>
 
       <!-- 遠端視訊 -->
-      <div class="w-full flex justify-center">
-        <div class="relative w-full md:w-[48%] max-w-[500px] aspect-video min-w-0">
-          <video ref="remoteVideo" class="w-full h-full object-cover bg-black rounded-xl shadow overflow-hidden"
-            autoplay playsinline></video>
-          <!-- <button v-if="localStream" @click="enterFullscreen(remoteVideo.value)"
+      <div class="relative w-full md:w-[48%] max-w-[500px] aspect-video min-w-0">
+        <video ref="remoteVideo" class="w-full h-full object-cover bg-black rounded-xl shadow overflow-hidden" autoplay
+          playsinline></video>
+        <!-- <button v-if="localStream" @click="enterFullscreen(remoteVideo.value)"
             class="absolute bottom-2 right-2 bg-white bg-opacity-70 text-black text-base px-4 py-2 rounded-xl hover:bg-opacity-90 shadow transition">
             ⛶ 全螢幕
           </button> -->
-        </div>
       </div>
     </div>
 
